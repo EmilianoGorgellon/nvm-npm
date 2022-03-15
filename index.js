b534c68093b927c51eca4c10367b6a490561be56
Emiliano <emilianogorgellon49@gmail.com>
2022-03-14 21:58:32 -0300

    Segundo

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-const express = require("express");
-const app = express();
-const PORT = 3000;
+// const express = require("express");
+// const app = express();
+// const PORT = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Estamos en yarn pa");
-})
+// app.get("/", (req, res) => {
+//     res.send("Estamos en yarn pa");
+// })
 
-app.listen(PORT);
+
+
+// app.listen(PORT);
+
+class Operaciones {
+    constructor() {
+
+    }
+    suma (num1, num2) {
+        return num1 + num2;
+    }
+    resta (num1, num2) {
+        return num1-num2;
+    }
+    multiplicacion (num1, num2) {
+        return num1 * num2;
+    }
+    division (num1, num2) {
+        return num1 / num2;
+    }
+}
+
+module.exports = new Operaciones();
